22ad044019349444b5dfb2fde65c43b931edf272
chalwe mwansa <chalwemwansa23@gmail.com>
2024-09-03 14:47:34 +0200

    modified:   src/components/home/home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,12 @@
 import "./home.css"; // the file containing the styles for the html in the code in this file
+import { Link } from "react-router-dom";
 
 // sample code, you can remove everything in return statement and put your own html
 export default function Home() {
   return (
     <div>
       <t>this is the zambia farm web</t>
+      <Link></Link> {/** used for linking pages */}
     </div>
   );
 }
